5702e254ab6071446334b4be8d43b0d520f5387c
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-11-13 13:58:00 +0530

    Cards added using props

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -13,12 +13,12 @@ function Header(){
                     <ul className="list">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">Services</a></li>
-                        <li><a href="#">About me</a></li>
-                        <li><a href="#">Protfolio</a></li>
-                        <li><a href="#">Contact me</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Restaurants</a></li>
+                        <li><a href="#">Contact us</a></li>
                     </ul>
                 </div>
-                <a href="" className="hire-me">Hire Me</a>
+                <a href="" className="login">Login</a>
                 <span id="more">&#9776;</span>
                 </nav>
         </header>
